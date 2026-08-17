9581b2f1141c6ae44e4ece5771f5e5bf4e15eefb
Satyam Tiwari <sttiwari9211@gmail.com>
2026-08-17 16:48:07 +0530

    Time: 8 ms (5.45%), Space: 52.3 MB (5.75%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -2,38 +2,28 @@ class Solution {
     public String addBinary(String a, String b) {
         int carry = 0;
         String finalSum = "";
-        int i = 0 , j = 0 ;
-        for( i = a.length() - 1, j = b.length() -1; i >= 0||j>=0; i--,j--){
-            int digitA = 0 , digitB = 0 , sum = 0;
+        int i = a.length() -1;
+        int j = b.length() - 1;
+        while(i >= 0 || j >= 0){
+            int digitA = 0, digitB = 0 , sum = 0;
             if(i >= 0){
-                digitA = a.charAt(i) - '0';
+                digitA = a.charAt(i) -'0';
             }else{
                 digitA = 0;
             }
-            if(j>=0){
+            if(j >= 0){
                 digitB = b.charAt(j) - '0';
             }else{
                 digitB = 0;
             }
-
             sum = digitA + digitB + carry;
+            int digit = sum % 2;
+            carry = sum /2;
 
-            if(sum == 0){
-                carry = 0;
-                finalSum = finalSum + sum;
-            }
-            else if(sum == 1){
-                carry =0;
-                finalSum = finalSum + sum;
-            }
-            else if(sum == 2){
-                carry =1;
-                finalSum = finalSum + "0";
-            }
-            else{
-                carry =1;
-                finalSum = finalSum + "1";
-            }
+            finalSum = finalSum + digit;
+
+            i--;
+            j--;
         }
         if(carry == 1){
             finalSum = finalSum + "1";
